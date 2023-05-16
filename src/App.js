33033a5d7507c7ca04a861c8536fb8c3cae5208d
KyleAktr <aktr.contact@gmail.com>
2023-05-16 10:31:47 +0200

    header done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import HeaderInfos from "./components/HeaderInfos";
+import GlobalChart from "./components/GlobalChart";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [coinsData, setCoinsData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y"
+      )
+      .then((res) => setCoinsData(res.data));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="app-container">
+        <header>
+          <HeaderInfos />
+          <GlobalChart coinsData={coinsData} />
+        </header>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
